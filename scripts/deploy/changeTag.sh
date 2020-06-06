c0dba987fbc55a9065918d927e243f65e1ef606a
Nohier <nohierassan2@gmail.com>
2020-06-06 17:56:23 +0200

    Fixing Directory 2

--- scripts/deploy/changeTag.sh
+++ scripts/deploy/changeTag.sh
@@ -3,5 +3,5 @@
 ##    a script to automate the deployment of the image to kubernetes cluster
 WORKSPACE="/home/non/Documents/Jenkins Course/PipeLineProject/src/jenkins_home/workspace/java-pipeline"
 
-sed "s/tagVersion/$1/g" $WORKSPACE/scripts/deploy/java-app-pod-defenition.yml > java-app-pod.yml
+sed "s/tagVersion/$1/g" $WORKSPACE+"/scripts/deploy/java-app-pod-defenition.yml" > java-app-pod.yml
  
